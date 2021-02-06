c6284755319da8c6f0cdf9774c35cc23bd56ae7b
andres <velasco.andrs@gmail.com>
2021-02-05 19:11:36 -0500

    auth button

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
- 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,11 +10,37 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text('AuthApp -- Google'),
+          actions: [],
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  splashColor: Colors.transparent,
+                  minWidth: double.infinity,
+                  height: 40,
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(FontAwesomeIcons.google, color: Colors.white,),
+                      Text(
+                        '  Sign in with google',
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
